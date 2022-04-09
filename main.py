23e7b96c977f05f9b2bcee3a8685035acfbf08c7
EF <acfogarty@users.noreply.github.com>
2022-04-09 22:08:57 +0200

    inherit from designer class, add event listener on textarea

--- main.py
+++ main.py
@@ -46,37 +46,49 @@ class Worker(QObject):
         self.finished.emit()
 
 
-class Main:
+class SprinterMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, app, parent=None):
 
-    def __init__(self, app):
+        super(SprinterMainWindow, self).__init__(parent)
+        self.setupUi(self)
 
         self.app = app
 
         # Force the style to be the same on all OSs:
         self.app.setStyle("Fusion")
+
         palette = make_darktheme_palette()
         self.app.setPalette(palette)
 
-        self.ui = None
+        self.start_button.clicked.connect(self.start_session_in_thread)
+        self.textarea.textChanged.connect(self.update_textchanged_time)
+        self.textarea.installEventFilter(self)
+
         self.session = None
 
-    def launch(self, MainWindow):
-        """
-        Setup UI, slots and signals on first launch
-        """
+    # def eventFilter(self, source, event):
+    #     if event.type() == QtCore.QEvent.FocusIn and source is self.ledit_corteA:
+    #         print("A")
+    #         self.flag = 0
+    #     if event.type() == QtCore.QEvent.FocusIn and source is self.ledit_corteB:
+    #         print("B")
+    #         self.flag = 1
+    #     return super(MainWindow, self).eventFilter(source, event)
 
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(MainWindow)
-        self.ui.start_button.clicked.connect(self.start_session_in_thread)
-        self.ui.textarea.textChanged.connect(self.update_textchanged_time)
+    def eventFilter(self, obj, event):
+        if event.type() == QtCore.QEvent.KeyPress and obj is self.textarea:
+            if event.key() == QtCore.Qt.Key_Return and self.textarea.hasFocus():
+                print('Enter pressed')
+        return super().eventFilter(obj, event)
+        
 
     def start_session_in_thread(self):
 
-        minutes_per_sprint = self.ui.minutes_per_sprint_spinbox.value()
-        target_wordcount = self.ui.target_wordcount_spinbox.value()
-        severity = self.ui.severity_slider.value()
+        minutes_per_sprint = self.minutes_per_sprint_spinbox.value()
+        target_wordcount = self.target_wordcount_spinbox.value()
+        severity = self.severity_slider.value()
 
-        initial_text_string = self.ui.textarea.toPlainText()
+        initial_text_string = self.textarea.toPlainText()
 
         text = Text(initial_text_string=initial_text_string)
         self.session = Session(minutes_per_sprint=minutes_per_sprint,
@@ -103,12 +115,12 @@ class Main:
         self.thread.start()
 
         # Final resets
-        self.ui.start_button.setEnabled(False)
+        self.start_button.setEnabled(False)
         self.thread.finished.connect(
-            lambda: self.ui.start_button.setEnabled(True)
+            lambda: self.start_button.setEnabled(True)
         )
         #self.thread.finished.connect(
-        #    lambda: self.ui.wordcount_value_label.setText("Long-Running Step: 0")
+        #    lambda: self.wordcount_value_label.setText("Long-Running Step: 0")
         #)
 
     def update_status(self):
@@ -117,7 +129,7 @@ class Main:
 
         if (self.session.seconds_remaining > 0) and (self.session.words_remaining > 0):
 
-            current_text_string = self.ui.textarea.toPlainText()
+            current_text_string = self.textarea.toPlainText()
 
             # update all session variables
             self.session.update_session_status(current_text_string)
@@ -148,11 +160,11 @@ class Main:
         perc_wc_achieved = self.session.perc_wc_achieved
 
         tr_text = f'{minutes_remaining}:00'
-        self.ui.time_remaining_value_label.setText(tr_text)
-        self.ui.time_remaining_progressBar.setProperty("value", perc_time_remaining)
+        self.time_remaining_value_label.setText(tr_text)
+        self.time_remaining_progressBar.setProperty("value", perc_time_remaining)
         wc_text = f'{added_wordcount} / {self.session.target_wordcount}'
-        self.ui.wordcount_value_label.setText(wc_text)
-        self.ui.wordcount_progressBar.setProperty("value", perc_wc_achieved)
+        self.wordcount_value_label.setText(wc_text)
+        self.wordcount_progressBar.setProperty("value", perc_wc_achieved)
     
     def check_alarm_condition(self):
         """
@@ -180,14 +192,22 @@ class Main:
         if self.session:
             self.session.time_lastmodified_textarea = time.time()
 
+    def backup_text_to_disk(self):
+        """
+        Save contents of textarea to temporary file on disk
+        """
+
+        current_text_string = self.textarea.toPlainText()
+
+        with open('sprinter.bkp.txt', 'w') as f:
+            f.write(current_text_string)
+
 
 if __name__ == "__main__":
 
     app = QtWidgets.QApplication(sys.argv)
 
-    MainWindow = QtWidgets.QMainWindow()
-    main = Main(app)
-    main.launch(MainWindow)
-    MainWindow.show()
+    window = SprinterMainWindow(app)
+    window.show()
     
     sys.exit(app.exec_())
